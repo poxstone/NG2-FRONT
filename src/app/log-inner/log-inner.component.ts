0307ac5b675cd615c4f2e1f75c46e443d6a615f7
oscar ortiz <oscar.ortiz@eforcers.com>
2017-06-13 22:34:19 -0500

    prepare design and add submenu

---
+++ src/app/log-inner/log-inner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log-inner',
+  templateUrl: './log-inner.component.html',
+  styleUrls: ['./log-inner.component.css']
+})
+export class LogInnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
